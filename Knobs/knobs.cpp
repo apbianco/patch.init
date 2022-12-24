b420867672aeca2a155aa330075ec4f4a3dc8019
apbianco <apbianco@gmail.com>
2022-12-24 23:32:32 +0100

    Calibration
    - Try to adjust the output range to be 0 to PRECISION.
    
    It's not quite there yet.

--- Knobs/knobs.cpp
+++ Knobs/knobs.cpp
@@ -8,22 +8,35 @@ using namespace daisysp;
 
 DaisyPatchSM dpsm;
 
-#define PRECISION 1000.0
+#define PRECISION 10000.0
 
 #define F2I(f) static_cast<int>(PRECISION * f)
+#define I2F(i) static_cast<float>(i / PRECISION)
 
 class KnobValue {
 public:
-  KnobValue(int cv_index) : prev_(0.0f) {
+  KnobValue(int cv_index, float min_value, float max_value) :
+    prev_(0.0f),
+    threshold_(min_value),
+    factor_(1/(max_value - min_value)) {
+    assert (max_value - min_value != 0.0);
     cv_index_ = cv_index;
-    dpsm.ProcessAnalogControls();
-    prev_ = dpsm.GetAdcValue(cv_index_);
+    prev_ = GetValue();
+  }
+
+  void Calibrate() {
+    dpsm.PrintLine("CV_%d: %d", cv_index_+1, F2I(GetRawValue()));
   }
   
-  bool MaybeCaptureNewValue(float *new_value) {
-    dpsm.ProcessAnalogControls();
-    *new_value = dpsm.GetAdcValue(cv_index_);
-    if (F2I(*new_value) != F2I(prev_)) {
+  bool CaptureValueIndicateChange(float *new_value) {
+    *new_value = GetValue();
+    int new_int_value = F2I(*new_value);
+    if (new_int_value < 0) {
+      new_int_value = 0;
+      *new_value = 0.0;
+    }
+    int delta = new_int_value - F2I(prev_);
+    if (delta <= -2 || delta >= 2) {
       prev_ = *new_value;
       return true;
     }
@@ -32,7 +45,7 @@ public:
 
   void PrintIfChange() {
     float value;
-    if (MaybeCaptureNewValue(&value)) {
+    if (CaptureValueIndicateChange(&value)) {
       Print();
     }
   }
@@ -43,7 +56,18 @@ public:
   
 private:
   float prev_;
+  float threshold_;
+  float factor_;
   int cv_index_;
+
+  float GetRawValue() {
+    dpsm.ProcessAnalogControls();
+    return dpsm.GetAdcValue(cv_index_);
+  }
+
+  float GetValue() {
+    return (GetRawValue() - threshold_) * factor_;
+  }
 };
 
 int main(void)
@@ -52,11 +76,11 @@ int main(void)
   dpsm.StartLog(true);		// Wait for the terminal connection
   dpsm.PrintLine("// Started...");
 
-  KnobValue kv(CV_1);
+  KnobValue kv(CV_2, I2F(-192), I2F(9961));
 
   while(true) {
     float f;
-    if (kv.MaybeCaptureNewValue(&f)) {
+    if (kv.CaptureValueIndicateChange(&f)) {
       dpsm.WriteCvOut(CV_OUT_2, f*5.0);
       kv.Print();
     }
