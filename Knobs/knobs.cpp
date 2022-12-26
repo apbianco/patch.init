7c016d329779b4373e0ad4df15b557fe0d66207e
apbianco <apbianco@gmail.com>
2022-12-26 16:15:40 +0100

    Hacking the Knobs/ directory
    - All 4 knobs more or less calibrated 0/500/999

--- Knobs/knobs.cpp
+++ Knobs/knobs.cpp
@@ -8,13 +8,19 @@ using namespace daisy;
 using namespace patch_sm;
 using namespace daisysp;
 
+struct KnobCalibrationValues {
+  float true_min;
+  float true_med;
+  float true_max;
+};
+
 class KnobValue {
 public:
-  KnobValue(int cv_index,
-	    float true_min, float true_max, int factor=1000) :
+  KnobValue(int cv_index, KnobCalibrationValues kcv, int factor=1000) :
     prev_(0.0f), cv_index_(cv_index),
-    true_min_(true_min),
-    correction_(1.0f/(true_max - true_min)),
+    true_min_(kcv.true_min), true_med_(kcv.true_med),
+    correction_low_(0.5f/(kcv.true_med - kcv.true_min)),
+    correction_high_(0.5f/(kcv.true_max - kcv.true_med)),
     calibrate_min_(10.0), calibrate_max_(-10.0),
     factor_(factor), debug_(false) {
     prev_ = CalibratedValue(GetValue());
@@ -43,9 +49,6 @@ public:
 	       f2a(fvalue), f2a(prev_), value, prev);
     }
     if (value != prev) {
-      if (debug_) {
-	LOG_INFO("Found different: %d/%d", value, prev);
-      }
       *new_value = prev_ = fvalue;
       return true;
     }
@@ -68,7 +71,8 @@ private:
   float prev_;
   int cv_index_;
 
-  float true_min_, correction_;
+  float true_min_, true_med_;
+  float correction_low_, correction_high_;
   float calibrate_min_, calibrate_max_;
 
   int factor_;
@@ -81,18 +85,35 @@ private:
   }
 
   float CalibratedValue(float f) {
-    return (f - true_min_)*correction_;
+    if (f < true_med_) {
+      return (f - true_min_)*correction_low_;
+    } else {
+      return 0.5 + (f - true_med_)*correction_high_;
+    }
   }
 };
 
 int main(void)
 {
+  KnobCalibrationValues kcv[4] = {
+    {-0.01260, 0.46364, 0.99676},
+    {-0.01940, 0.46142, 0.99630},
+    {-0.01379, 0.46813, 0.99758},
+    {-0.01611, 0.46630, 0.99911}};
+      
   InitHardware(true);
-  KnobValue kv(CV_2, -0.01940, 0.99630);
+  KnobValue kv[4] = {
+    KnobValue(CV_1, kcv[0]),
+    KnobValue(CV_2, kcv[1]),
+    KnobValue(CV_3, kcv[2]),
+    KnobValue(CV_4, kcv[3]),
+  };
 
   while(true) {
-#if 0
-    kv.PrintIfChange();
+#if 1
+    for (int i = 0; i < 4; i++) {
+      kv[i].PrintIfChange();
+    }
 #else
     kv.Calibrate();
 #endif    
