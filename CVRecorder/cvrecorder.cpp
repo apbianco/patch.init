e1ce254c7621aaa3efa9e5e48ada8b92f3aed720
apbianco <apbianco@gmail.com>
2022-12-28 22:15:56 +0100

    This can reliably register a long press.

--- CVRecorder/cvrecorder.cpp
+++ CVRecorder/cvrecorder.cpp
@@ -4,14 +4,29 @@
 #include "cv.h"
 #include "knobs.h"
 #include "led.h"
+#include "push_button.h"
 
 using namespace daisy;
 using namespace patch_sm;
 
 int main(void) {
   InitHardware(true);
-  
+
+  OnOffPushButton button;
+  OnOffPushButton::State button_state;
+
+  LED led;
+  led.Blink(3);
+  button.SetLED(&led);
   while(true) {
-    System::Delay(5);
+    if (button.GetStateIfChanged(&button_state)) {
+      button.Print();
+      if (button_state.state == OnOffPushButton::StateValue::ON &&
+	  button_state.long_press) {
+	LOG_INFO("Start recording");
+      }
+    }
+    LOG_INFO_EVERY_MS(2000, "Alive...");
+    System::Delay(0);
   }
 }
