f752c16bea62c67d64cd544447312c2837c30345
apbianco <apbianco@gmail.com>
2022-12-28 12:33:28 +0100

    control_voltage.h is renamed cv.h

--- util/cv.h
+++ util/cv.h
@@ -1,14 +1,14 @@
 #ifndef CONTROL_VOLTAGE_H
 #define CONTROL_VOLTAGE_H
 
-#include "knobs.h"
+#include "cv_.h"
 
 class CVIn : public CV_ {
  public:
  CVIn(int cv_index, LinearCalibrationValues kcv, int factor=1000) :
   CV_(cv_index, factor) {
-    l1_ = Linearizer(kcv.true_min, kcv.true_med, -5.0f, 0.0f);
-    l2_ = Linearizer(kcv.true_med, kcv.true_max, 0.0f, 5.0f);
+    l1_ = Transcaler(kcv.true_min, kcv.true_med, -5.0f, 0.0f);
+    l2_ = Transcaler(kcv.true_med, kcv.true_max, 0.0f, 5.0f);
   }
 };
 
