f14b18b6d5cbfb018627da8f2184bcd76e63aa18
apbianco <apbianco@gmail.com>
2022-12-28 09:53:16 +0100

    Adjustment to the control voltage classes.
    - Adjust for the Knob name change
    - Create a simple CVOut class.

--- util/control_voltage.h
+++ util/control_voltage.h
@@ -3,16 +3,33 @@
 
 #include "knobs.h"
 
-class CVIn : public KnobValue {
+class CVIn : public Knob {
  public:
  CVIn(int cv_index, LinearCalibrationValues kcv, int factor=1000) :
-  KnobValue(cv_index, kcv, factor) {
+  Knob(cv_index, kcv, factor) {
     l1_ = Linearizer(kcv.true_min, kcv.true_med, -5.0f, 0.0f);
     l2_ = Linearizer(kcv.true_med, kcv.true_max, 0.0f, 5.0f);
   }
-  void PrintCalibrated() {
+  void Print() {
     LOG_INFO("CV_%d: %s", cv_index_+1, f2a(prev_));
   }
+};
+
+class CVOut {
+ public:
+ CVOut() : debug_(false) {}
+
+  void SetVoltage(float v) {
+    GetHardware()->WriteCvOut(CV_OUT_1, v);
+    if (debug_) {
+      LOG_INFO("CV_OUT_1: %s", f2a(v));
+    }
+  }
+
+  void SetDebug() { debug_ = true; }
+  
+ private:
+  bool debug_;
 };
 
 #endif  // CONTROL_VOLTAGE_H
