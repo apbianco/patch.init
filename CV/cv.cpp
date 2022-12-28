e84036e8e28b3bae757068ddd93d68d3fd878c25
apbianco <apbianco@gmail.com>
2022-12-28 16:52:09 +0100

    CVIn changes
    - Define CreateCVIn<N> methods
    - Use one in the CV example code.

--- CV/cv.cpp
+++ CV/cv.cpp
@@ -8,18 +8,11 @@
 using namespace daisy;
 using namespace patch_sm;
 
-LinearCalibrationValues kcv_[4] = {
-  {-0.97918, -0.01791, 0.99993 },
-  {-0.97909, -0.00659, 0.99990 },
-  {-0.97711,  0.01214, 0.99990 },
-  {-0.96890,  0.01174, 0.99987 },
-};
-
 int main(void)
 {
   InitHardware(true);
   CVOut cv_out;
-  Knob k(CV_1, kcv_[0]);
+  Knob k = CreateKnob1();
   
   while(true) {
     k.PrintIfChange();
--- util/cv.h
+++ util/cv.h
@@ -29,4 +29,20 @@ class CVOut {
   bool debug_;
 };
 
+CVIn CreateCVIn1() {
+  return CVIn(CV_1, {-0.97918, -0.01791, 0.99993 });
+}
+
+CVIn CreateCVIn2() {
+  return CVIn(CV_2, {-0.97909, -0.00659, 0.99990 });
+}
+
+CVIn CreateCVIn3() {
+  return CVIn(CV_3, {-0.97711,  0.01214, 0.99990 });
+}
+
+CVIn CreateCVIn4() {
+  return CVIn(CV_4, {-0.96890,  0.01174, 0.99987 });
+}
+
 #endif  // CONTROL_VOLTAGE_H
