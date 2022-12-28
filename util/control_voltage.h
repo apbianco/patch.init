1c403d8759f395a42d1869a1f99730e2329f6379
apbianco <apbianco@gmail.com>
2022-12-28 11:16:47 +0100

    Print() method should be picked up from the upper class.

--- util/control_voltage.h
+++ util/control_voltage.h
@@ -10,9 +10,6 @@ class CVIn : public Knob {
     l1_ = Linearizer(kcv.true_min, kcv.true_med, -5.0f, 0.0f);
     l2_ = Linearizer(kcv.true_med, kcv.true_max, 0.0f, 5.0f);
   }
-  void Print() {
-    LOG_INFO("CV_%d: %s", cv_index_+1, f2a(prev_));
-  }
 };
 
 class CVOut {
