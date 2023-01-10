2119af54da9cc779dbe403d7c4306fcde08ef35f
apbianco <apbianco@gmail.com>
2023-01-10 13:38:29 +0100

    Define gate wrapper classes.
    - Define Gate_ as a base class
    - InGate and OutGate inherit from it. Don't use GateIn as it would clash with an already defined class
    -  Add CreateInGate<n> and CreateOutGate<n> helper functions.

--- util/gate.h
+++ util/gate.h
@@ -1,22 +1,101 @@
 #ifndef GATE_H
 #define GATE_H
 
-class GateOut {
+#include <daisy_patch_sm.h>
+#include <daisy.h>
+
+#include "hid/gatein.h"
+
+class Gate_ {
+ public:  
   enum State {
     ON,
-    OFF
+    OFF,
+    UNKNOWN,
   };
+  Gate_(int32_t gate_index) :
+    gate_index_(gate_index),
+    last_transition_time_(System::GetNow()), state_(OFF),
+    initialized_(false), debug_(false) {
+    if (gate_index_ < 1 || gate_index_ > 2) {
+      LOG_WARN("Gate index %d out of range, defaulting to 1", gate_index_);
+      gate_index_ = 1;
+    }
+  }
+
+  virtual ~Gate_() = default;
+
+  void SetDebug() { debug_ = true; }
+
+  inline State GetState() { return state_; }
+
+  virtual void Print() {}
+
+protected:
+
+  inline State AdjustInternalState(State s) {
+    state_ = s;
+    last_transition_time_ = System::GetNow();
+    if (debug_) {
+      Print();
+    }
+    return state_;
+  }
+  
+  int32_t gate_index_;
+  uint32_t last_transition_time_;
+  State state_;
+  bool initialized_;
+  bool debug_;
+
+  const char* StateName(State state) {
+    switch (state) {
+    case ON:
+      return "ON";
+    case OFF:
+      return "OFF";
+    case UNKNOWN:
+      return "UNKNOWN";
+    }
+    return "???";
+  }
+};
+
+class InGate : public Gate_ {
  public:
-  GateOut(int gate_index):
-    gate_index_(gate_index), state_(OFF),
-    last_transition_time_(System::GetNow()), debug_(false) {}
+  InGate(int gate_index): Gate_(gate_index) {}
+
+  void Init() {
+    gate_.Init(gate_index_ == 1 ? 
+	       (dsy_gpio_pin *)&DaisyPatchSM::B10 : 
+	       (dsy_gpio_pin *)&DaisyPatchSM::B9, true);
+    GetState();
+    initialized_ = true;
+  }
+
+  State GetState() {
+    if (!initialized_) {
+      LOG_ERROR("Can't get state, not initialized");
+      return UNKNOWN;
+    }
+    return AdjustInternalState(gate_.State() ? ON : OFF);
+  }
+  
+  void Print() {
+    LOG_INFO("InGate #%d: state:  %s", gate_index_, StateName(state_));
+  }
+
+private:
+  GateIn gate_;
+};
+
+class OutGate : public Gate_ {
+ public:
+  OutGate(int gate_index): Gate_(gate_index) {}
 
   void Init() {
     gate_.mode = DSY_GPIO_MODE_OUTPUT_PP;
     gate_.pull = DSY_GPIO_NOPULL;
-    if (gate_index_ < 1 || gate_index_ > 2) {
-      LOG_WARN("Gate index %d out of range", gate_index_);
-    }
     // FIXME: This should be fixed with
     // https://github.com/electro-smith/libDaisy/pull/558 being pulled
     // in.
@@ -24,19 +103,11 @@ class GateOut {
 		 DaisyPatchSM::B6 : DaisyPatchSM::B5);
     dsy_gpio_init(&gate_);
     SetState(OFF);
+    initialized_ = true;
   }
   
-  void On() {
-    SetState(ON);
-  }
-
-  void Off() {
-    SetState(OFF);
-  }
-
-  State GetState() {
-    return state_;
-  }
+  inline void On() { SetState(ON); }
+  inline void Off() { SetState(OFF); }
 
   void Alternate(float delay_ms = 500) {
     if (System::GetNow() - last_transition_time_ > delay_ms) {
@@ -44,32 +115,29 @@ class GateOut {
     }
   }
 
-  void Print() {
-    LOG_INFO("Gate #%d: state: %s", gate_index_,
-	     state_ == ON ? "ON" : "OFF");
-  }
-
   inline void SetState(State state) {
-    dsy_gpio_write(&gate_, state == ON ? true : false);
-    last_transition_time_ = System::GetNow();
-    state_ = state;
-    if (debug_) {
-      Print();
+    if (!initialized_) {
+      LOG_ERROR("Can't set state, not initialized");
+      return;
     }
+    dsy_gpio_write(&gate_, state == ON ? true : false);
+    AdjustInternalState(state);
   }
 
-  void SetDebug() { debug_ = true; }
-  
- private:
-  int32_t gate_index_;
+  void Print() {
+    LOG_INFO("OutGate #%d: state: %s", gate_index_, StateName(state_));
+  }
+
+private:
   dsy_gpio gate_;
-  State state_;
-  uint32_t last_transition_time_;
-  bool debug_;
 };
 
-GateOut CreateGateOut1() { return GateOut(1); }
+InGate CreateInGate1() { return InGate(1); }
 
-GateOut CreateGateOut2() { return GateOut(2); }
+InGate CreateInGate2() { return InGate(2); }
 
-#endif
+OutGate CreateOutGate1() { return OutGate(1); }
+
+OutGate CreateOutGate2() { return OutGate(2); }
+
+#endif  // GATE_H
