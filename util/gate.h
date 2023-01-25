2fd372f851054dcf36ad538bf2619363674ad3fc
apbianco <apbianco@gmail.com>
2023-01-25 20:39:40 +0100

    Add the ability to generate a pulse.
    You get control on the temporal width and rest time.

--- util/gate.h
+++ util/gate.h
@@ -104,8 +104,16 @@ private:
 
 class OutGate : public Gate_ {
  public:
-  OutGate() = default;
-  OutGate(int gate_index): Gate_(gate_index) {}
+  enum PulseState {
+    ARMED,
+    STARTED,
+    DONE,
+    ARMABLE,
+  };
+  OutGate() : OutGate(-1) {}
+  OutGate(int gate_index):
+    Gate_(gate_index), pulse_state_(ARMABLE),
+    pulse_length_ms_(5), pulse_rest_ms_(1) {}
 
   void Init() {
     gate_.mode = DSY_GPIO_MODE_OUTPUT_PP;
@@ -134,16 +142,85 @@ class OutGate : public Gate_ {
       LOG_ERROR("Can't set state, not initialized");
       return;
     }
-    dsy_gpio_write(&gate_, state == ON ? true : false);
-    AdjustInternalState(state);
+    if (state_ != state) {
+      dsy_gpio_write(&gate_, state == ON ? true : false);
+      AdjustInternalState(state);
+    }
+  }
+
+  // Arm the pulse. Return true of the pulse could be
+  // armed. Otherwise, it means the pulse can not be armed (yet) and
+  // ArmPulse() has to be called later to return true.
+  inline bool ArmPulse(uint32_t pulse_length, uint32_t pulse_rest=1) {
+    if (pulse_state_ == ARMABLE) {
+      pulse_state_ = ARMED;
+      pulse_length_ms_ = pulse_length;
+      pulse_rest_ms_ = pulse_rest;
+      return true;
+    }
+    return false;
+  }
+
+  // Generate a pulse - this is meant to be called periodically so
+  // that the internal state can be adjusted. Return true when the
+  // pulse just ended.
+  //
+  // ARMARBLE ---- ArmPulse ------> ARMED
+  //   ^                               |
+  //   | pulse rest elapsed            | UpdatePulse
+  //   |                               v
+  // DONE <- pulse length elapsed - STARTED
+  
+  inline bool UpdatePulse() {
+    auto time_in_state_ms = System::GetNow() - last_transition_time_;
+    switch (pulse_state_) {
+    case ARMED:
+      On();
+      pulse_state_ = STARTED;
+      break;
+    case STARTED:
+      if (time_in_state_ms > pulse_length_ms_) {
+	Off();
+	pulse_state_ = DONE;
+	return true;
+      }
+      break;
+    case DONE:
+      if (time_in_state_ms > pulse_rest_ms_) {
+	pulse_state_ = ARMABLE;
+      }
+      break;
+    case ARMABLE:
+      break;
+    }
+    return false;
   }
 
   void Print() {
-    LOG_INFO("OutGate #%d: state: %s", gate_index_, StateName(state_));
+    LOG_INFO("OutGate #%d: state: %s, pulse state: %s",
+	     gate_index_, StateName(state_), PulseStateName(pulse_state_));
   }
 
 private:
+  
+  const char* PulseStateName(PulseState state) {
+    switch (state) {
+    case ARMED:
+      return "ARMED";
+    case STARTED:
+      return "STARTED";
+    case DONE:
+      return "DONE";
+    case ARMABLE:
+      return "ARMABLE";
+    }
+    return "???";
+  }
+
   dsy_gpio gate_;
+  PulseState pulse_state_;
+  uint32_t pulse_length_ms_;
+  uint32_t pulse_rest_ms_;
 };
 
 InGate CreateInGate1() { return InGate(1); }
